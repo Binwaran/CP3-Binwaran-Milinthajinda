c12e638c405c9a1d13f92cd0034a4ea4c3f4404e
Binwaran <73376855+Binwaran@users.noreply.github.com>
2021-01-02 14:07:11 +0800

    Add Exercise 4
    Add Exercise 4 to github

---
+++ assignments/Exercise4_Binwaran_M.py
@@ -0,0 +1,11 @@
+fe = 85
+gb = 82
+ics = 90
+cp = 87
+
+print("-----Your Score-----")
+print("Foundation English",":",fe)
+print("General Business",":",gb)
+print("Introduction to Computer Systems",":",ics)
+print("Computer Programming",":",cp)
+
