135078f914563eee572374d395de2a7ba8d04913
Binwaran <73376855+Binwaran@users.noreply.github.com>
2021-01-09 19:49:08 +0800

    Add Lecture50_Binwaran_M.py
    Add Lecture50_Binwaran_M.py

---
+++ assignments/Lecture50_Binwaran_M.py
@@ -0,0 +1,18 @@
+def addNumber(x,y):
+    print(x+y)
+
+def minusNumber(x,y):
+    print(x-y)
+
+def multiplyNumber(x,y):
+    print(x*y)
+
+def divideNumber(x,y):
+    print(x/y)
+
+addNumber(50,29)
+minusNumber(50,29)
+multiplyNumber(50,29)
+divideNumber(50,29)
+
+
